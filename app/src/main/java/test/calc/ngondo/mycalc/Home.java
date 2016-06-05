378ac70d72e9f3270ee610032c8be19bae8e7ba0
cngondo <devngondo@gmail.com>
2016-06-06 02:34:07 +0300

    Added Layout components

--- app/src/main/java/test/calc/ngondo/mycalc/Home.java
+++ app/src/main/java/test/calc/ngondo/mycalc/Home.java
@@ -3,6 +3,9 @@ package test.calc.ngondo.mycalc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import calc.operations.Operations;
 
@@ -12,6 +15,20 @@ public class Home extends AppCompatActivity implements Operations, View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //all view elements here
+        final TextView lblTitle = (TextView) findViewById(R.id.titleApp);
+        final TextView lblFirstNumber = (TextView) findViewById(R.id.fnumber);
+        final TextView lblSecondNumber= (TextView) findViewById(R.id.lnumber);
+
+        final EditText mFirstNumber = (EditText) findViewById(R.id.firstNumber);
+        final EditText mSecondNumber = (EditText) findViewById(R.id.secondNumber);
+        final EditText manswer = (EditText) findViewById(R.id.answer);
+
+        Button btnAdd = (Button) findViewById(R.id.buttonAdd);
+        Button btnSubtract = (Button) findViewById(R.id.buttonSubtract);
+        Button btnDivide = (Button) findViewById(R.id.buttonDivide);
+        Button btnMultiply = (Button) findViewById(R.id.buttonMultiply);
+
     }
 
     @Override
